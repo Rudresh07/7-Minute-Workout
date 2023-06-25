43b7b0b51350afd170ed93120f7aff9bb13e3534
Rudresh07 <rudreshpatel01bhu10@gmail.com>
2023-06-25 09:44:04 +0530

    Adding excercise page

--- app/src/main/java/com/example/a7minuteworkout/ExcerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExcerciseActivity.kt
@@ -3,12 +3,19 @@ package com.example.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.example.a7minuteworkout.databinding.ActivityExcerciseBinding
 
 class ExcerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExcerciseBinding?= null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress =0
+
+    private var ExcerciseTimer: CountDownTimer? = null
+    private var ExcerciseProgress =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,31 +37,99 @@ class ExcerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        progressTimer()
+        setRestView()
+        }
+
+
+
+    private fun setRestView(){
+
+
+
+        if (restTimer!=null){
+            restTimer?.cancel()
+            restProgress=0
+        }
+
+        progressRestTimer()
+
     }
 
-    fun progressTimer() {
+    private fun SetExcerciseView(){
 
+        binding?.flTimer?.visibility = View.INVISIBLE
+        binding?.tv2ndScreen?.text = "Excercise Name"
+        binding?.flExcerciseTimer?.visibility = View.VISIBLE
+        if (ExcerciseTimer!=null){
+            ExcerciseTimer?.cancel()
+            ExcerciseProgress=0
+        }
+
+        progressExcerciseTimer()
+    }
+
+
+    private fun progressRestTimer() {
+ binding?.progressBar?.progress = restProgress
         val totalDuration: Long = 10000
 
         object : CountDownTimer(totalDuration, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                val progressBar = binding?.progressBar
+
+                restProgress++
+                binding?.progressBar?.progress = 10-restProgress
 
                 val secondLeft = millisUntilFinished / 1000
                 binding?.timerCount?.text = secondLeft.toString()
 
-                val progress = (( millisUntilFinished) * 100 / totalDuration).toInt()
-                progressBar?.progress = progress
+
+
             }
 
             override fun onFinish() {
-                binding?.timerCount?.text = "0"
-                binding?.progressBar?.progress = 100
+                SetExcerciseView()
+
+            }
+        }.start()
+    }
+
+
+
+    fun progressExcerciseTimer() {
+        binding?.excerciseProgressBar?.progress = ExcerciseProgress
+        val totalDuration: Long = 30000
+
+        object : CountDownTimer(totalDuration, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                ExcerciseProgress++
+                binding?.excerciseProgressBar?.progress = 30-ExcerciseProgress
+
+                val secondLeft = millisUntilFinished / 1000
+                binding?.timerCountExcercise?.text = secondLeft.toString()
+
+
+
+            }
+
+            override fun onFinish() {
+
                 Toast.makeText(applicationContext, "Timer finished", Toast.LENGTH_SHORT).show()
             }
         }.start()
     }
 
+    override fun onDestroy() {
+
+
+        if (restTimer!=null){
+            restTimer?.cancel()
+            restProgress=0
+        }
+
+
+        super.onDestroy()
+        binding =null
+    }
 
     }
